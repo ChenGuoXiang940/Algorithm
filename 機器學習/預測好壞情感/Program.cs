c54c760b29ee2d34b5027adb4d5a0a47f9f1da1f
陳國翔 <chen199940@gmail.com>
2023-08-21 10:35:11 +0800

    新增

---
+++ 機器學習/預測好壞情感/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace 機器學習訓練
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var sampleData = new MLModel1.ModelInput()
+                {
+                    Col0 = Console.ReadLine() + ""
+                };
+                var result = MLModel1.Predict(sampleData);
+                var sentiment = result.PredictedLabel == 1 ? "Positive" : "Negative";
+                Console.WriteLine($"Text: {sampleData.Col0}\nSentiment: {sentiment}");
+            }
+        }
+    }
+}
