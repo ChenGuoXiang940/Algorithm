58cef6b714984ad3f1b769994c346b8e8771cf8e
陳國翔 <chen199940@gmail.com>
2023-08-24 22:57:49 +0800

    新增

---
+++ 機器學習/旅遊商問題TSP/Class1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using f = 旅遊商問題TSP.Form1;
+namespace 旅遊商問題TSP
+{
+    internal class Data
+    {
+        public static Random rnd = new Random();
+        public static int rd() => rnd.Next(0, 25);
+        public static Func<Point, Point, double> GetDistance = (Point p1, Point p2) => Math.Abs(Math.Pow(p1.X - p2.X, 2) + Math.Pow(p1.Y - p2.Y, 2));
+        public static double count_energy(ref int[] conf)
+        {
+            double temp = 0;
+            for (int i = 1; i < 25; ++i)
+            {
+                temp += GetDistance(f.col[conf[i]], f.col[conf[i - 1]]);
+            }
+            temp += GetDistance(f.col[conf[0]], f.col[conf[25 - 1]]);
+            return temp;
+        }
+        public static bool metro(double f1, double f2, double t)
+        {
+            if (f2 < f1) return true;
+            double p = Math.Exp(-(f2 - f1) / t);
+            double bignum = 1E9;
+            if (rnd.Next(0, 36768) % bignum < p * bignum) return true;
+            return false;
+        }
+        public static void generate(ref int[] s)//隨機產生一組新解
+        {
+            bool[] vist = new bool[25];
+            for (int i = 0; i < 25; ++i)
+            {
+                s[i] = rd();
+                while (vist[s[i]])
+                {
+                    s[i] = rd();
+                }
+                vist[s[i]] = true;
+            }
+        }
+        public static void generate1(ref int[] s)//隨機交換序列中的一組城市順序
+        {
+            int ti = rd();
+            int tj = ti;
+            while (ti == tj)
+            {
+                tj = rd();
+            }
+            for (int i = 0; i < 25; i++)
+            {
+                s[i] = f.seq[i];
+            }
+            swap(ref s[ti],ref s[tj]);
+        }
+        public static void generate2(ref int[] s)//隨機交換序列中的兩組城市順序
+        {
+            int ti = rd();
+            int tj = ti;
+            int tk = ti;
+            while (ti == tj)
+            {
+                tj = rd();
+            }
+            while (ti == tj || tj == tk || ti == tk)
+            {
+                tk = rd();
+            }
+            for (int i = 0; i < 25; ++i)
+            {
+                s[i] = f.seq[i];
+            }  
+            swap(ref s[ti], ref s[tj]);
+            swap(ref s[tk], ref s[tj]);
+        }
+        public static void generate3(ref int[] s)//隨機選序列中的三個城市互相交換順序
+        {
+            int ti = rd();
+            int tj = ti;
+            int tm = rd();
+            int tn = ti;
+            while (ti == tj)
+            {
+                tj = rd();
+            }
+            while (tm == tn)
+            {
+                tn = rd();
+            }
+            for (int i = 0; i < 25; ++i)
+            {
+                s[i] = f.seq[i];
+            }
+            swap(ref s[ti], ref s[tj]);
+            swap(ref s[tm], ref s[tn]);
+        }
+        public static void generate0(ref int[] s)//以上三種交換方式等概率選擇
+        {
+            switch (f.rnd.Next(0, 3))
+            {
+                case 0:
+                    generate1(ref s);
+                    break;
+                case 1:
+                    generate2(ref s);
+                    break;
+                case 2:
+                    generate3(ref s);
+                    break;
+            }
+        }
+        public static void swap<T>(ref T a, ref T b)
+        {
+            T tmp = a; a = b; b = tmp;
+        }
+    }
+}
---
+++ 機器學習/旅遊商問題TSP/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace 旅遊商問題TSP
+{
+    public partial class Form1 : Form
+    {
+        public static Bitmap bitmap;
+        public static Graphics g;
+        public static Pen pen = new Pen(Color.Blue, 3);
+        public static Font font = new Font("Default", 9);
+        public Form1()
+        {
+            InitializeComponent();
+            bitmap = new Bitmap(500, 500);
+            g = Graphics.FromImage(bitmap);
+            g.Clear(Color.White);
+            pictureBox1.Image = bitmap;
+        }
+        public static Random rnd = new Random();
+        public static List<Point> col = new List<Point>();
+        public static Func<Point, Point, double> GetDistance = (Point p1, Point p2) => Math.Abs(Math.Pow(p1.X - p2.X, 2) + Math.Pow(p1.Y - p2.Y, 2));
+        private void button1_Click(object sender, EventArgs e)
+        {
+            col.Clear();
+            for(int i = 0; i < 25; i++)
+            {
+                int x = rnd.Next(10, 490);
+                int y = rnd.Next(10, 490);
+                if (!col.Contains(new Point(x, y))) col.Add(new Point(x, y));
+            }
+            reset();
+        }
+        public void reset()
+        {
+            g.Clear(Color.White);
+            for (int i = 0; i < 25; i++)
+            {
+                g.FillEllipse(Brushes.Blue, col[i].X - 5, col[i].Y - 5, 10, 10);
+            }
+            pictureBox1.Image = bitmap;
+        }
+        public static int[] seq;  //最短路徑
+        public static int[] seq_f;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            seq = new int[25];
+            seq_f = new int[25];
+            double tempterature = 10000;//初始溫度
+            double result = 1E9;        //最短路徑長度
+            double new_energy = 1, old_energy = 0;
+            double[,] distance = new double[25, 25];
+            for (int i = 0; i < 25; i++)
+            {
+                for(int j = 0; j < 25; j++)
+                {
+                    distance[i, j] = i == j ? 0 : GetDistance(col[i], col[j]);
+                }
+            }
+            for(int i = 0; i < 25; i++)
+            {
+                seq[i] = seq_f[i] = i;
+            }
+            while (tempterature > 1E-9 && Math.Abs(new_energy - old_energy) > 1E-9)
+            {
+                int iterate = 100;
+                int[] seq_ff = new int[25];
+                while (iterate-- >= 0 && Math.Abs(new_energy - old_energy) > 1E-9)
+                {
+                    Data.generate1(ref seq_ff);
+                    new_energy = Data.count_energy(ref seq_ff);
+                    old_energy = Data.count_energy(ref seq_f);
+                    if (Data.metro(old_energy, new_energy, tempterature))
+                    {
+                        for (int i = 0; i < 25;)
+                        {
+                            seq_f[i] = seq_ff[i++];
+                        }
+                    }
+                }
+                new_energy = Data.count_energy(ref seq_f);
+                old_energy = result;
+                if (Data.metro(old_energy, new_energy, tempterature))
+                {
+                    for (int i = 0; i < 25;)
+                    {
+                        seq[i] = seq_f[i++];
+                    }
+                    tempterature *= 0.998;
+                }
+                else
+                {
+                    tempterature *= 0.999;
+                }
+                result = Data.count_energy(ref seq);
+            }
+            reset();
+            g.DrawString("0", font, Brushes.Black, col[seq[0]]);
+            g.DrawLine(pen, col[seq[0]], col[seq[24]]);
+            for (int i = 1; i < 25; i++)
+            {
+                g.DrawLine(pen, col[seq[i]], col[seq[i - 1]]);
+                g.DrawString($"{i}", font, Brushes.Black, col[seq[i]]);
+            }
+            pictureBox1.Image = bitmap;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
