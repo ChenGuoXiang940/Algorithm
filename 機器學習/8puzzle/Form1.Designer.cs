37f02d74a962794c8ddee1fa8b77c4f264232a9b
陳國翔 <chen199940@gmail.com>
2023-08-25 14:24:23 +0800

    新增

---
+++ 機器學習/8puzzle/Form1.Designer.cs
@@ -0,0 +1,408 @@
+﻿namespace _8puzzle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tbb9 = new System.Windows.Forms.TextBox();
+            this.tbb8 = new System.Windows.Forms.TextBox();
+            this.tbb7 = new System.Windows.Forms.TextBox();
+            this.tbb6 = new System.Windows.Forms.TextBox();
+            this.tbb5 = new System.Windows.Forms.TextBox();
+            this.tbb4 = new System.Windows.Forms.TextBox();
+            this.tbb3 = new System.Windows.Forms.TextBox();
+            this.tbb2 = new System.Windows.Forms.TextBox();
+            this.tbb1 = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.tb9 = new System.Windows.Forms.TextBox();
+            this.tb8 = new System.Windows.Forms.TextBox();
+            this.tb7 = new System.Windows.Forms.TextBox();
+            this.tb6 = new System.Windows.Forms.TextBox();
+            this.tb5 = new System.Windows.Forms.TextBox();
+            this.tb4 = new System.Windows.Forms.TextBox();
+            this.tb3 = new System.Windows.Forms.TextBox();
+            this.tb2 = new System.Windows.Forms.TextBox();
+            this.tb1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("新細明體", 12F);
+            this.button1.Location = new System.Drawing.Point(12, 248);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 54);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "隨機";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("新細明體", 12F);
+            this.button2.Location = new System.Drawing.Point(154, 248);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(136, 54);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "開始";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("新細明體", 12F);
+            this.button3.Location = new System.Drawing.Point(296, 248);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(136, 54);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "離開";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.tbb9);
+            this.panel1.Controls.Add(this.tbb8);
+            this.panel1.Controls.Add(this.tbb7);
+            this.panel1.Controls.Add(this.tbb6);
+            this.panel1.Controls.Add(this.tbb5);
+            this.panel1.Controls.Add(this.tbb4);
+            this.panel1.Controls.Add(this.tbb3);
+            this.panel1.Controls.Add(this.tbb2);
+            this.panel1.Controls.Add(this.tbb1);
+            this.panel1.Font = new System.Drawing.Font("新細明體", 12F);
+            this.panel1.Location = new System.Drawing.Point(12, 58);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(153, 169);
+            this.panel1.TabIndex = 3;
+            // 
+            // tbb9
+            // 
+            this.tbb9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbb9.Font = new System.Drawing.Font("Consolas", 12F);
+            this.tbb9.Location = new System.Drawing.Point(108, 118);
+            this.tbb9.Name = "tbb9";
+            this.tbb9.Size = new System.Drawing.Size(30, 36);
+            this.tbb9.TabIndex = 17;
+            this.tbb9.Text = "8";
+            this.tbb9.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tbb8
+            // 
+            this.tbb8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbb8.Font = new System.Drawing.Font("Consolas", 12F);
+            this.tbb8.Location = new System.Drawing.Point(59, 118);
+            this.tbb8.Name = "tbb8";
+            this.tbb8.Size = new System.Drawing.Size(30, 36);
+            this.tbb8.TabIndex = 16;
+            this.tbb8.Text = "7";
+            this.tbb8.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tbb7
+            // 
+            this.tbb7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbb7.Font = new System.Drawing.Font("Consolas", 12F);
+            this.tbb7.Location = new System.Drawing.Point(13, 118);
+            this.tbb7.Name = "tbb7";
+            this.tbb7.Size = new System.Drawing.Size(30, 36);
+            this.tbb7.TabIndex = 15;
+            this.tbb7.Text = "6";
+            this.tbb7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tbb6
+            // 
+            this.tbb6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbb6.Font = new System.Drawing.Font("Consolas", 12F);
+            this.tbb6.Location = new System.Drawing.Point(108, 67);
+            this.tbb6.Name = "tbb6";
+            this.tbb6.Size = new System.Drawing.Size(30, 36);
+            this.tbb6.TabIndex = 14;
+            this.tbb6.Text = "5";
+            this.tbb6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tbb5
+            // 
+            this.tbb5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbb5.Font = new System.Drawing.Font("Consolas", 12F);
+            this.tbb5.Location = new System.Drawing.Point(59, 67);
+            this.tbb5.Name = "tbb5";
+            this.tbb5.Size = new System.Drawing.Size(30, 36);
+            this.tbb5.TabIndex = 13;
+            this.tbb5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tbb4
+            // 
+            this.tbb4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbb4.Font = new System.Drawing.Font("Consolas", 12F);
+            this.tbb4.Location = new System.Drawing.Point(13, 67);
+            this.tbb4.Name = "tbb4";
+            this.tbb4.Size = new System.Drawing.Size(30, 36);
+            this.tbb4.TabIndex = 12;
+            this.tbb4.Text = "4";
+            this.tbb4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tbb3
+            // 
+            this.tbb3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbb3.Font = new System.Drawing.Font("Consolas", 12F);
+            this.tbb3.Location = new System.Drawing.Point(108, 13);
+            this.tbb3.Name = "tbb3";
+            this.tbb3.Size = new System.Drawing.Size(30, 36);
+            this.tbb3.TabIndex = 11;
+            this.tbb3.Text = "3";
+            this.tbb3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tbb2
+            // 
+            this.tbb2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbb2.Font = new System.Drawing.Font("Consolas", 12F);
+            this.tbb2.Location = new System.Drawing.Point(59, 13);
+            this.tbb2.Name = "tbb2";
+            this.tbb2.Size = new System.Drawing.Size(30, 36);
+            this.tbb2.TabIndex = 10;
+            this.tbb2.Text = "2";
+            this.tbb2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tbb1
+            // 
+            this.tbb1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbb1.Font = new System.Drawing.Font("Consolas", 12F);
+            this.tbb1.Location = new System.Drawing.Point(13, 13);
+            this.tbb1.Name = "tbb1";
+            this.tbb1.Size = new System.Drawing.Size(30, 36);
+            this.tbb1.TabIndex = 9;
+            this.tbb1.Text = "1";
+            this.tbb1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.tb9);
+            this.panel2.Controls.Add(this.tb8);
+            this.panel2.Controls.Add(this.tb7);
+            this.panel2.Controls.Add(this.tb6);
+            this.panel2.Controls.Add(this.tb5);
+            this.panel2.Controls.Add(this.tb4);
+            this.panel2.Controls.Add(this.tb3);
+            this.panel2.Controls.Add(this.tb2);
+            this.panel2.Controls.Add(this.tb1);
+            this.panel2.Font = new System.Drawing.Font("新細明體", 12F);
+            this.panel2.Location = new System.Drawing.Point(237, 58);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(152, 169);
+            this.panel2.TabIndex = 4;
+            // 
+            // tb9
+            // 
+            this.tb9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tb9.Font = new System.Drawing.Font("Consolas", 12F);
+            this.tb9.Location = new System.Drawing.Point(108, 118);
+            this.tb9.Name = "tb9";
+            this.tb9.Size = new System.Drawing.Size(30, 36);
+            this.tb9.TabIndex = 8;
+            this.tb9.Text = "8";
+            this.tb9.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tb8
+            // 
+            this.tb8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tb8.Font = new System.Drawing.Font("Consolas", 12F);
+            this.tb8.Location = new System.Drawing.Point(59, 118);
+            this.tb8.Name = "tb8";
+            this.tb8.Size = new System.Drawing.Size(30, 36);
+            this.tb8.TabIndex = 7;
+            this.tb8.Text = "7";
+            this.tb8.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tb7
+            // 
+            this.tb7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tb7.Font = new System.Drawing.Font("Consolas", 12F);
+            this.tb7.Location = new System.Drawing.Point(13, 118);
+            this.tb7.Name = "tb7";
+            this.tb7.Size = new System.Drawing.Size(30, 36);
+            this.tb7.TabIndex = 6;
+            this.tb7.Text = "6";
+            this.tb7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tb6
+            // 
+            this.tb6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tb6.Font = new System.Drawing.Font("Consolas", 12F);
+            this.tb6.Location = new System.Drawing.Point(108, 67);
+            this.tb6.Name = "tb6";
+            this.tb6.Size = new System.Drawing.Size(30, 36);
+            this.tb6.TabIndex = 5;
+            this.tb6.Text = "5";
+            this.tb6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tb5
+            // 
+            this.tb5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tb5.Font = new System.Drawing.Font("Consolas", 12F);
+            this.tb5.Location = new System.Drawing.Point(59, 67);
+            this.tb5.Name = "tb5";
+            this.tb5.Size = new System.Drawing.Size(30, 36);
+            this.tb5.TabIndex = 4;
+            this.tb5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tb4
+            // 
+            this.tb4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tb4.Font = new System.Drawing.Font("Consolas", 12F);
+            this.tb4.Location = new System.Drawing.Point(13, 67);
+            this.tb4.Name = "tb4";
+            this.tb4.Size = new System.Drawing.Size(30, 36);
+            this.tb4.TabIndex = 3;
+            this.tb4.Text = "4";
+            this.tb4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tb3
+            // 
+            this.tb3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tb3.Font = new System.Drawing.Font("Consolas", 12F);
+            this.tb3.Location = new System.Drawing.Point(108, 13);
+            this.tb3.Name = "tb3";
+            this.tb3.Size = new System.Drawing.Size(30, 36);
+            this.tb3.TabIndex = 2;
+            this.tb3.Text = "3";
+            this.tb3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tb2
+            // 
+            this.tb2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tb2.Font = new System.Drawing.Font("Consolas", 12F);
+            this.tb2.Location = new System.Drawing.Point(59, 13);
+            this.tb2.Name = "tb2";
+            this.tb2.Size = new System.Drawing.Size(30, 36);
+            this.tb2.TabIndex = 1;
+            this.tb2.Text = "2";
+            this.tb2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tb1
+            // 
+            this.tb1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tb1.Font = new System.Drawing.Font("Consolas", 12F);
+            this.tb1.Location = new System.Drawing.Point(13, 13);
+            this.tb1.Name = "tb1";
+            this.tb1.Size = new System.Drawing.Size(30, 36);
+            this.tb1.TabIndex = 0;
+            this.tb1.Text = "1";
+            this.tb1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 12F);
+            this.label1.Location = new System.Drawing.Point(34, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 24);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Initial State";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 12F);
+            this.label2.Location = new System.Drawing.Point(292, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 24);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Goal";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("新細明體", 12F);
+            this.label3.Location = new System.Drawing.Point(407, 58);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 24);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(546, 341);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "8-Puzzle System 智慧盤系統";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox tb1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbb9;
+        private System.Windows.Forms.TextBox tbb8;
+        private System.Windows.Forms.TextBox tbb7;
+        private System.Windows.Forms.TextBox tbb6;
+        private System.Windows.Forms.TextBox tbb5;
+        private System.Windows.Forms.TextBox tbb4;
+        private System.Windows.Forms.TextBox tbb3;
+        private System.Windows.Forms.TextBox tbb2;
+        private System.Windows.Forms.TextBox tbb1;
+        private System.Windows.Forms.TextBox tb9;
+        private System.Windows.Forms.TextBox tb8;
+        private System.Windows.Forms.TextBox tb7;
+        private System.Windows.Forms.TextBox tb6;
+        private System.Windows.Forms.TextBox tb5;
+        private System.Windows.Forms.TextBox tb4;
+        private System.Windows.Forms.TextBox tb3;
+        private System.Windows.Forms.TextBox tb2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ 機器學習/8puzzle/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using li = System.Collections.Generic.List<int>;
+namespace _8puzzle
+{
+    public partial class Form1 : Form
+    {
+        public static TextBox[] t1, t2;
+        public static List<li> moved = new List<li>() { new li() { 1, 3 }, new li() { 4, 2, 0 }, new li() { 5, 1 },
+        new li(){6,4,0 },new li(){ 7,5,1,3},new li(){8,2,4 },new li(){7,3 },new li(){ 8,4,6},new li(){ 5,7} };
+        public Form1()
+        {
+            InitializeComponent();
+            t1 = new TextBox[] { tbb1, tbb2, tbb3, tbb4, tbb5, tbb6, tbb7, tbb8, tbb9 };
+            t2 = new TextBox[] { tb1, tb2, tb3, tb4, tb5, tb6, tb7, tb8, tb9 };
+        }
+        public static Random rnd = new Random();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            Array.ForEach(t1, item => item.Text = i++ == 0 ? "" : $"{i - 1}");
+            for(int j = 0; j < 10; j++)
+            {
+                swap(ref t1[rnd.Next(0, 9)], ref t1[rnd.Next(0, 9)]);
+            }
+        }
+        public void enable(bool fg)
+        {
+            button1.Enabled = fg;
+            button2.Enabled = fg;
+            Array.ForEach(t1, item => item.Enabled = fg);
+            Array.ForEach(t2, item => item.Enabled = fg);
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            enable(false);
+            Stopwatch sw = Stopwatch.StartNew();
+            sw.Start();
+            int start = 0, final = 0, power = 1, target = 0;
+            for (int i = 8; i >= 0; i--)
+            {
+                if (t1[i].Text == "") target = 8 - i;
+                else start += power * int.Parse(t1[i].Text);
+                final += power * int.Parse(t2[i].Text == "" ? "0" : t2[i].Text);
+                power *= 10;
+            }
+            HashSet<double> visited = new HashSet<double>();
+            Queue<Data> queue = new Queue<Data>();
+            queue.Enqueue(new Data(start, target));
+            visited.Add(start);
+            int len;
+            Data result = null;
+            while ((len = queue.Count) > 0)
+            {
+                for (int j = 0; j < len; j++)
+                {
+                    Data current = queue.Dequeue();
+                    if (current.val == final)
+                    {
+                        result = current;
+                        queue.Clear();
+                        break;
+                    }
+                    foreach(var move in moved[current.index])
+                    {
+                        int val_new = swapDigits(current.val, current.index, move);
+                        if (visited.Contains(val_new)) continue;
+                        queue.Enqueue(new Data(val_new, move, current));
+                        visited.Add(val_new);
+                    }
+                }
+            }
+            sw.Stop();
+            if (result == null)
+            {
+                label3.Text = $"無解\r\n耗時:{Math.Round(sw.Elapsed.TotalSeconds, 3)}";
+                enable(true);
+                return;
+            }
+            Stack<Data> stack = new Stack<Data>();
+            while (result != null)
+            {
+                stack.Push(result);
+                result = result.father;
+            }
+            string record = (stack.Count - 1).ToString();
+            label3.Text = $"{record}/{record}";
+            Thread.Sleep(500);
+            Application.DoEvents();
+            while (true)
+            {
+                Thread.Sleep(500);
+                Application.DoEvents();
+                if (stack.Count == 0) break;
+                double current = stack.Pop().val;
+                for(int i = 0; i < 9; i++)
+                {
+                    t1[8 - i].Text = current % 10 < 0.99 ? "" : $"{(int)current % 10}";
+                    current /= 10;
+                }
+                label3.Text = $"{stack.Count,-2}/{record}";
+            }
+            enable(true);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        public void swap(ref TextBox a,ref TextBox b)
+        {
+            string tmp=a.Text; a.Text=b.Text; b.Text = tmp;
+        }
+        public static int[] power = new int[] { 1, (int)1E1, (int)1E2, (int)1E3, (int)1E4, (int)1E5, (int)1E6, (int)1E7, (int)1E8 };
+        public int swapDigits(int num, int pos1, int pos2)
+        {
+            int m1 = power[pos1];
+            int m2 = power[pos2];
+            int digit1 = (num / m1) % 10;
+            int digit2 = (num / m2) % 10;
+            num -= (m1 * digit1 + m2 * digit2);
+            return num + (m1 * digit2 + m2 * digit1);
+        }
+    }
+    public class Data
+    {
+        public int val, index;
+        public Data father;
+        public Data(int val,int index, Data father = null)
+        {
+            this.val = val;
+            this.index = index;
+            this.father = father;
+        }
+    }
+}
