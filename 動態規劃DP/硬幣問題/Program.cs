bde74ac4c84a925b574841b5483531cc10fff42c
陳國翔 <chen199940@gmail.com>
2023-08-29 16:15:53 +0800

    整理

--- 動態規劃DP/硬幣問題/Program.cs
+++ 動態規劃DP/硬幣問題/Program.cs
@@ -4,10 +4,10 @@
     {
         static void Main(string[] args)
         {
-            //時間複雜度 O(amount * coins.length)
+            //時間複雜度 O(amount * coins.length) 硬幣數量無限
             //在計算組合總數時非常高效，適用於類似找零、組合等問題
             //該演算法並不需要回溯搜索，因此它與回溯法和剪枝法無關
-            int amount = 100;
+            int amount = 10;
             int[] coins = new int[] { 1, 5, 10, 50 };
             int[] dp = new int[amount + 1];
             dp[0] = 1;
