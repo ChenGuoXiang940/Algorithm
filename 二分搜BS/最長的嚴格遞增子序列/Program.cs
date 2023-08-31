d6cfc9c662da91804626d8d1408191993a9f410d
陳國翔 <chen199940@gmail.com>
2023-08-31 15:00:49 +0800

    修正

--- 二分搜BS/最長的嚴格遞增子序列/Program.cs
+++ 二分搜BS/最長的嚴格遞增子序列/Program.cs
@@ -10,25 +10,28 @@ namespace 最長的嚴格遞增子序列
             //tails[i] 表示長度為 i+1 的遞增子序列的最末元素的最小值
             //遍歷到一個新的數字時，我們將其插入到 tails 陣列中適當的位置，從而保證 tails 陣列始終是遞增
             int[] nums = new int[] { 20, 40, 32, 67, 40, 20, 89, 300, 404, 13, 13 };
-            List<int> tails = new List<int>() { 0 };
-            foreach (int num in nums)
+            List<int> tails = new List<int>() { nums[0] };
+            for(int i=1;i<nums.Length; i++)
             {
-                int left = 0, right = tails.Count - 1;
-                while (left < right)
-                {
-                    int mid = left + (right - left) / 2;
-                    if (tails[mid] < num)
-                        left = mid + 1;
-                    else
-                        right = mid;
-                }
-                tails[left] = num;
-                Console.WriteLine($"num:{num} left:{left} tails.Count-1:{tails.Count - 1} {(left == tails.Count - 1 ? "新增" : "")}");
                 //找到了一個比 tails 串列中的所有元素都大的 num，遞增子序列的末端新增一個元素
-                if (left == tails.Count - 1) tails.Add(0);
+                if (tails.Last() < nums[i])
+                {
+                    tails.Add(nums[i]);
+                }
+                else
+                {
+                    int left = 0;
+                    int right = tails.Count - 1;
+                    while (left < right)
+                    {
+                        int mid = left + (right - left) / 2;
+                        if (tails[mid] < nums[i]) left = mid + 1;
+                        else right = mid;
+                    }
+                    tails[left] = nums[i];
+                }
             }
-            Console.WriteLine(tails.Count - 1);
-            tails.RemoveAt(tails.Count - 1);
+            Console.WriteLine(tails.Count);
             Console.WriteLine(string.Join(",", tails.ToArray()));
             Console.ReadKey();
         }
---
+++ 二分搜BS/開根號/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace 開根號
+{
+    internal class Program
+    {
+        public static long bs(long left,long right,long key)
+        {
+            if (left > right) return right;
+            long mid = left + (right - left) / 2;
+            if (mid * mid == key) return mid;
+            else if (mid * mid < key) return bs(mid + 1, right, key);
+            else return bs(left, mid - 1, key);
+        }
+        static void Main(string[] args)
+        {
+            int x = 8;
+            Console.WriteLine(bs(0, 25536, x));
+            Console.ReadKey();
+        }
+    }
+}
