2960e54904c16d52b2d6b376dcff6d2c2deccf71
陳國翔 <chen199940@gmail.com>
2023-10-03 10:40:34 +0800

    新增

---
+++ 圖graph/kruskal最小生成樹/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace kruskal最小生成樹
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Data> col = new List<Data>();
+            HashSet<char> set = new HashSet<char>();
+            col.Add(new Data('A', 'B', 6));
+            col.Add(new Data('A', 'F', 12));
+            col.Add(new Data('A', 'E', 10));
+            col.Add(new Data('B', 'F', 8));
+            col.Add(new Data('B', 'D', 5));
+            col.Add(new Data('B', 'C', 3));
+            col.Add(new Data('D', 'C', 7));
+            col.Add(new Data('E', 'D', 9));
+            col.Add(new Data('F', 'D', 11));
+            col = col.OrderBy(item => item.cost).ToList();
+            int res = 0;
+            foreach(var item in col)
+            {
+                if (set.Contains(item.s) && set.Contains(item.d)) continue;
+                Console.WriteLine($"{item.s}-{item.d}");
+                res += item.cost;
+                set.Add(item.s);
+                set.Add(item.d);
+            }
+            Console.WriteLine($"最小花費:{res}");
+            Console.ReadKey();
+        }
+    }
+    class Data
+    {
+        public char s, d;
+        public int cost;
+        public Data(char s, char d, int cost)
+        {
+            this.s = s;
+            this.d = d;
+            this.cost = cost;
+        }
+    }
+}
---
+++ 線段樹Segment Tree/315Count_of_Smaller_Numbers_After_Self/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace _315Count_of_Smaller_Numbers_After_Self
+{
+    class Seg
+    {
+        private int len;
+        public int[] arr, count;
+        private List<int> orgindex;
+        private void build(int left,int right)
+        {
+            if (left>= right) return;
+            int mid = (left + right) >> 1;
+            build(left, mid);
+            build(mid + 1, right);
+            Console.WriteLine(left + "#" + mid + "#" + right);
+            merge(left, mid, right);
+        }
+        private void merge(int left,int mid,int right)
+        {
+            int ls = mid - left + 1;
+            int rs = right - mid;
+            List<int> list_left = orgindex.GetRange(left, ls);
+            List<int> list_right = orgindex.GetRange(mid + 1, rs);
+            int i = 0, j = 0, k = left, jump = 0;
+            while (i < ls && j < rs)
+            {
+                if (arr[list_left[i]] <= arr[list_right[j]])
+                {
+                    orgindex[k] = list_left[i];
+                    count[list_left[i]] += jump;
+                    i++;
+                }
+                else
+                {
+                    orgindex[k] = list_right[j];
+                    jump++;
+                    j++;
+                }
+                k++;
+            }
+
+            while (i < ls)
+            {
+                orgindex[k] = list_left[i];
+                count[list_left[i]] += jump;
+                i++;
+                k++;
+            }
+
+            while (j < rs)
+            {
+                orgindex[k] = list_right[j];
+                j++;
+                k++;
+            }
+        }
+        public Seg(int[]nums)
+        {
+            len = nums.Length;
+            count = new int[len];
+            arr = nums;
+            int i = 0;
+            orgindex = new int[len].Select(item => i++).ToList();
+            build(0, len - 1);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 5, 2, 6, 1 };
+            Seg seg = new Seg(nums);
+            Console.WriteLine(string.Join(" ", seg.count));
+            Console.ReadKey();
+        }
+    }
+}
