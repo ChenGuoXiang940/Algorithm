fa3cc0c859d354a1fac36411c920da9a1488f2a6
陳國翔 <chen199940@gmail.com>
2023-12-12 10:25:37 +0800

    新增

---
+++ 堆疊stack/堆疊/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 堆疊
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static Stack stack = new Stack();
+        public static Random rnd = new Random();
+        public static string getStr() => string.Join("", new string[3].Select(x => (char)rnd.Next(65, 91)));
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TextBox text = new TextBox() { Name = $"textBox{stack.sign}", Text = getStr(), Location = new Point(stack.x, 10), Size = new Size(50, 10) };
+            panel1.Controls.Add(text);
+            stack.Push(text);
+            textBox1.Text = $"message:新增{stack.Peek().Text}";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!stack.isEmpty())
+            {
+                textBox1.Text = $"message:移除{stack.Peek().Text}";
+                panel1.Controls.Remove(stack.Pop());
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = $"message:清除";
+            panel1.Controls.Clear();
+            stack.Clear();
+        }
+    }
+    public class Stack
+    {
+        public Stack() => list = new List<TextBox>();
+        private List<TextBox> list;
+        private int aim = 0;
+        public int sign = 1, x = 30;
+        public void Push(TextBox s)
+        {
+            if (list.Count == aim)
+            {
+                list.Add(new TextBox());
+            }
+            list[aim++] = s;
+            x += 60;
+            sign++;
+        }
+        public TextBox Pop()
+        {
+            x -= 60;
+            return list[--aim];
+        }
+        public TextBox Peek() => list[aim - 1];
+        public bool isEmpty() => aim == 0;
+        public void Clear()
+        {
+            list.Clear();
+            aim = 0;
+            x = 30;
+        }
+    }
+}
---
+++ 貪心greedy/水槽隔板畫圖/Q1/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Q1
+{
+    public partial class Form1 : Form
+    {
+        public static Bitmap bitmap = new Bitmap(500, 250);
+        public static Graphics g;
+        public static Pen pen = new Pen(Color.Black, 3);
+        public static Font font = new Font("Default", 9);
+        public static Point final;
+        public static Random rnd = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            g = Graphics.FromImage(bitmap);
+            
+        }
+        public void Reset()
+        {
+            g.Clear(Color.White);
+            g.DrawRectangle(pen, 25, 25, 450, 200);
+            for(int i = 0; i < 10; i++)
+            {
+                g.DrawString($"{i}", font, Brushes.Black, 10, 215 - 20 * i);
+                g.DrawLine(pen, 25, 225 - 20 * i, 30, 225 - 20 * i);
+                g.DrawString($"{i + 1}", font, Brushes.Black, 50 + 40 * i, 230);           
+            }
+            pictureBox1.Image = bitmap;
+        }
+        public static int water(int[]hs,int c)
+        {
+            int total = Int32.MaxValue;
+            int record;
+            for (int i = 0; i < hs.Length; i++)
+            {
+                record = 0;
+                for (int j = i + 1; j < hs.Length; j++)
+                {
+                    if (record < hs[j])
+                    {
+                        int a = Math.Min(hs[i], hs[j]) * (j - i);
+                        if (a == c)
+                        {
+                            final = new Point(i, j);
+                            return c;
+                        }
+                        else if (total > a && a > c)
+                        {
+                            final = new Point(i, j);
+                            total = a;
+                        }
+                    }
+                    record = Math.Max(record, hs[j]);
+                    if (hs[i] <= record) break;
+                }
+            }
+            if (total == Int32.MaxValue) return -1;
+            return total;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Reset();
+            int[] hs = textBox1.Text.Split(' ').Select(item => int.Parse(item)).ToArray();
+            int c = int.Parse(textBox3.Text);
+            int ans = water(hs, c);
+            if (ans != -1)
+            {
+                textBox2.Text = $"{ans}";
+                g.FillRectangle(Brushes.LightBlue, 50 + 40 * final.X, 225 - 20 * Math.Min(hs[final.X], hs[final.Y]), 40 * (final.Y - final.X), 20 * Math.Min(hs[final.X], hs[final.Y]));
+                for(int i = 0; i < hs.Length; i++)
+                {
+                    g.FillRectangle(Brushes.Red, 50 + 40 * i, 225 - 20 * hs[i], 5, 20 * hs[i]);
+                }
+                g.FillRectangle(Brushes.DarkBlue, 50 + 40 * final.X, 225 - 20 * hs[final.X], 5, 20 * hs[final.X]);
+                g.FillRectangle(Brushes.DarkBlue, 50 + 40 * final.Y, 225 - 20 * hs[final.Y], 5, 20 * hs[final.Y]);
+            }
+            else
+            {
+                for (int i = 0; i < hs.Length; i++)
+                {
+                    g.FillRectangle(Brushes.Red, 50 + 40 * i, 225 - 20 * hs[i], 5, 20 * hs[i]);
+                }
+                MessageBox.Show("無解","Hint");
+                textBox2.Text = "";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = string.Join(" ", new int[10].Select(x => rnd.Next(1, 10)));
+            textBox3.Text = $"{rnd.Next(5, 20)}";
+            button1_Click(sender, e);
+        }
+    }
+}
